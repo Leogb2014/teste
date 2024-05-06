a6edb6fe2d184bb9a547ec6f70506b54fd55ec37
Leonardo Gobetti <leonardogobetti@hotmail.com>
2024-05-06 18:48:00 -0300

    Teste

---
+++ src/testegit/TesteDeReversao.java
@@ -0,0 +1,16 @@
+package testegit;
+
+public class TesteDeReversao {
+
+	public static void main(String[] args) {
+		int numero = 0;
+		
+		if(numero != 10) {
+			System.out.println("Leo");
+		}else {
+			System.out.println("Não identificado");
+		}
+
+	}
+
+}
